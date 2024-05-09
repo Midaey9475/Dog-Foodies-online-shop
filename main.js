0f47491023cbe1d2bf690246048a0c197302a45d
Horlar9475 <festusakinboye@gmail.com>
2024-05-09 14:51:33 +0100

    update repo

--- main.js
+++ main.js
@@ -3,3 +3,19 @@ var addcrt = document.getElementById('addCrt')
 function adCrt(value){
     myCrt.innerHTML = eval(Number(myCrt.innerHTML)  + Number(value));
 }
+
+var hamburger = document.getElementById('hamburger')
+var burgerMenu = document.getElementById('burgerMenu')
+var closeMenu = document.getElementById('closeMenu')
+var burgerDrop = document.getElementById('burgerDrop')
+
+function openMenu(){
+    burgerDrop.style.display = 'grid'
+    burgerMenu.style.display = 'none'
+    closeMenu.style.display = 'block'
+}
+function closeburgerMenu(){
+    burgerDrop.style.display = 'none'
+    burgerMenu.style.display = 'grid'
+    closeMenu.style.display = 'none'
+}
